271caf2bfccdb75cc5b8c57557eabe77d5fe691a
Marin de Wild <marindewild@pop-os.localdomain>
2023-11-26 14:54:23 +0100

    all

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function showPopup(popupId) {
+    closeAllPopups();
     document.getElementById(popupId).style.display = 'block';
   }
   
   function closePopup(popupId) {
     document.getElementById(popupId).style.display = 'none';
+  }
+
+  function closeAllPopups() {
+    var popups = document.querySelectorAll('.popup');
+    popups.forEach(function (popup) {
+      popup.style.display = 'none';
+    });
   }
